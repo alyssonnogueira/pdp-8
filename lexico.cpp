dbbf7d438be618d7564ad810b4c7d50cbc2fcfc9
felipe <fdlopes@fdlopes-Vostro-5470.(none)>
2016-06-01 21:13:41 -0300

    Analisador Léxico parcial pronto. Com .cpp e .txt para testes

---
+++ lexico.cpp
@@ -0,0 +1,34 @@
+// Programa principal para testar o analisador léxico
+
+#include <iostream>
+#include <string.h>
+#include <fstream>
+
+// Inclui o Header da classe lexico.h
+#include "lexico.hpp"
+
+using namespace std;
+
+int main(){
+
+	Lexico lexico;
+	string retorno;
+
+	// Cria ponteiro e abre o arquivo de entrada
+	ifstream entrada;
+	entrada.open("teste.txt", ios_base::in);
+ 	if (!entrada){
+     		cout << "Não foi possível abrir o arquivo!" << endl;
+     		return 0;
+  	}
+
+  	if(!lexico.lerEntrada(&entrada)){ // Le o arquivo de entrada
+  		cout << "Não foi possivel ler o arquivo! " << endl;
+  	}
+
+  	lexico.imprimeLista();
+
+  	entrada.close();
+
+	return 0;
+}
