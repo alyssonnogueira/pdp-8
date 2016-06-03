6f08518fa4a75e70b206d31296a760566704ec81
Maxwell Moreira <maxwellburkemoreira@gmail.com>
2016-06-03 16:36:33 -0300

    Codigo alterado de lista para matriz

--- interpretador.cpp
+++ interpretador.cpp
@@ -13,7 +13,7 @@ using namespace std;
 Interpretador::Interpretador(){ }// Construtor default da classe
 
 int Interpretador::findStop(vector < vector<int> > listaComandos){
-
+/*
     // cout << "STOP > ";
     int i;
     for(i=0;i<listaComandos.size();i++){
@@ -22,16 +22,20 @@ int Interpretador::findStop(vector < vector<int> > listaComandos){
             return i;
         }
     }
+    */
 }
 
 void Interpretador::iniciaTabela(){
+
     int i;
     for(i=0;i<100;i++){
         symbolTable[i]=0;
     }
+
 }
 
 void Interpretador::constroiTabela (vector < vector<int> > listaComandos){
+   /*
     int stopPosition = findStop(listaComandos);
     iniciaTabela();
     int i;
@@ -42,114 +46,108 @@ void Interpretador::constroiTabela (vector < vector<int> > listaComandos){
             }
         }
     }
+    */
 }
 
 void Interpretador::processaCodigo(vector < vector<int> > listaComandos){
     ACC=0;
     int stopPosition = findStop(listaComandos);
+    iniciaTabela();
     constroiTabela(listaComandos);
     int i;// i simboliza o indereço de memoria
-        
-    for(i=0;i<listaComandos.size();i++){
-        cout << listaComandos[i]<<" ";
-        cout << listaComandos[i+1];
-        cout << " ACC: " << ACC << endl;
 
-        if(listaComandos[i] == 11){ //LDA
-            i++;
-            ACC = symbolTable[listaComandos[i]];
+    for(i=0;i<listaComandos.size();i++){
+        cout << listaComandos[i][0]<<" ";
+        cout << listaComandos[i][1];
+
+
+        if(listaComandos[i][0] == 11){ //LDA
+            ACC = symbolTable[listaComandos[i][1]];
         }
-        if(listaComandos[i] == 12 || listaComandos[i] == 00){//STA
-            if(listaComandos[i] == 00){
-            	i++;
-            	symbolTable[listaComandos[i]] = listaComandos[i];
+
+        if(listaComandos[i][0] == 12 || listaComandos[i][0] == 00){//STA
+            if(listaComandos[i][0] == 00){
+            	symbolTable[listaComandos[i][1]] = listaComandos[i][1];
             }
             else{
-            	i++;
-            	symbolTable[listaComandos[i]] = ACC;
+            	symbolTable[listaComandos[i][1]] = ACC;
             }
         }
-        if(listaComandos[i] == 21){//ADD
-            i++;
-            ACC += symbolTable[listaComandos[i]];
+        if(listaComandos[i][0] == 21){//ADD
+            ACC += symbolTable[listaComandos[i][1]];
         }
-        if(listaComandos[i] == 22){//SUB
-            i++;
-            ACC = ACC - symbolTable[listaComandos[i]];
+        if(listaComandos[i][0] == 22){//SUB
+            ACC = ACC - symbolTable[listaComandos[i][1]];
         }
-        if(listaComandos[i] == 23){//MULL
+        if(listaComandos[i][0] == 23){//MULL
             i++;
-            ACC = ACC * symbolTable[listaComandos[i]];
+            ACC = ACC * symbolTable[listaComandos[i][1]];
         }
-        if(listaComandos[i] == 24){//DIV
-            i++;
-            ACC = ACC / symbolTable[listaComandos[i]];
+        if(listaComandos[i][0] == 24){//DIV
+            ACC = ACC / symbolTable[listaComandos[i][1]];
         }
-        if(listaComandos[i] == 25){//REM
-            i++;
-            ACC = ACC%symbolTable[listaComandos[i]];
+        if(listaComandos[i][0] == 25){//REM
+            ACC = ACC%symbolTable[listaComandos[i][1]];
         }
-        if(listaComandos[i] == 29){//REV
+        if(listaComandos[i][0] == 29){//REV
             ACC = -ACC;
         }
-        if(listaComandos[i] == 31){//INN
-            i++;
+        if(listaComandos[i][0] == 31){//INN
             cout << endl;
             cout <<"Digite um valor: ";
-            cin >> symbolTable[listaComandos[i]];
+            cin >> symbolTable[listaComandos[i][1]];
             cout << endl;
-            cout << "Tabela: " << symbolTable[listaComandos[i]] << endl;
+            cout << "Tabela: " << symbolTable[listaComandos[i][1]] << endl;
             cout << endl;
         }
-        if(listaComandos[i] == 41){//PRN
-            i++;
+        if(listaComandos[i][0] == 41){//PRN
             cout << endl;
             cout <<"Valor: ";
-            cout << symbolTable[listaComandos[i]]<<endl;
+            cout << symbolTable[listaComandos[i][1]]<<endl;
         }
-        if(listaComandos[i] == 50){//NOP
+        if(listaComandos[i][0] == 50){//NOP
             //COMANDO VAZIO, APENAS PASSA PARA O PROXIMO
         }
-        if(listaComandos[i] == 51){//JMP
-            i = listaComandos[i+1]; //I  nesse caso está representando o indice no vetor de inteiros, que é o codigo
+        if(listaComandos[i][0] == 51){//JMP
+            i = listaComandos[i][1]-2; //I  nesse caso está representando o indice no vetor de inteiros, que é o codigo
         }
-        if(listaComandos[i] == 52){//JLE
+        if(listaComandos[i][0] == 52){//JLE
             if (ACC<=0)
-                i = listaComandos[i+1];
+                i = listaComandos[i][1]-2;
         }
-        if(listaComandos[i] == 53){//JDZ
+        if(listaComandos[i][0] == 53){//JDZ
             if (ACC!=0)
-                i = listaComandos[i+1];
+                i = listaComandos[i][1]-2;
         }
-       	if(listaComandos[i] == 54){//JGT
+       	if(listaComandos[i][0] == 54){//JGT
           	if (ACC>0)
-              	i = listaComandos[i+1];
+              	i = listaComandos[i][1]-2;
         }
-        if(listaComandos[i] == 55){//JEQ
+        if(listaComandos[i][0] == 55){//JEQ
            	if(ACC == 0)
-           	    i = listaComandos[i+1];
+           	    i = listaComandos[i][1]-2;
         }
-        if(listaComandos[i] == 56){//JLT
+        if(listaComandos[i][0] == 56){//JLT
 	       	if (ACC < 0){
-      	   		for(int k = i; k < listaComandos.size(); k++)
-               		if(listaComandos[k] == 41)
-                		i = k - 3;
-           	    	cout << i << endl;
+      	   		/*for(int k = i; k < listaComandos.size(); k++)
+               		if(listaComandos[k] == 41)*/
+                		i = listaComandos[i][1] - 2;
+           	    	//cout << i << endl;
            	}
            	else{
-               	i++;
            	}
         }
-        if(listaComandos[i] == 57){//JGE
+        if(listaComandos[i][0] == 57){//JGE
            	if (ACC >= 0)
-           	    i = listaComandos[i+1];
+           	    i = listaComandos[i][1] - 2;
         }
-        if(listaComandos[i] == 70){//
+        if(listaComandos[i][0] == 70){//
            	cout << endl;
            	cout << "FIM DE PROGRAMA" << endl;
            	cout << endl;
            	return;
         }
+        cout << " ACC: " << ACC << endl;
         getchar();
     }
 }
